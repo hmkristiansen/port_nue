e47f4916ca77a4cc63904c67b8a260d35394bf71
Hinken <henrkri@gmail.com>
2020-05-04 19:44:24 +0100

    almost finished

--- testing/script/script.js
+++ testing/script/script.js
@@ -1,6 +1,15 @@
 
 function fadeInPage(){
     $('body').removeClass('loadHide');
+    checkIfTouch();
+}
+
+
+function checkIfTouch(){
+	var touchsupport = ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)
+	if (!touchsupport){ // browser doesn't support touch
+		$('body').addClass('non-touch');
+	}
 }
 
 
